77fbb2b0fcf232ce46dee5ee641abad82ab0ac72
JJason-liu <765798166@qq.com>
2018-10-26 17:10:51 +0800

    首次提交

---
+++ src/main/java/mqtest/mqtest/App.java
@@ -0,0 +1,13 @@
+package mqtest.mqtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
